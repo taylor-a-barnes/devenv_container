a77bd6635477a75d77c089de56caf6d8b6abcdbd
Taylor Barnes <taylor.a.barnes@gmail.com>
2026-01-05 13:38:29 -0600

    Update treesitter.lua

--- .nvim/nvim/lua/plugins/treesitter.lua
+++ .nvim/nvim/lua/plugins/treesitter.lua
@@ -3,36 +3,26 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        -- List of parsers to install
-        ensure_installed = {
-          "bash",
-          "c",
-          "cmake",
-          "cpp",
-          "cuda",
-          "lua",
-          "python",
-          "rust",
-          "vim",
-          "vimdoc",
-          "query",
-          "markdown",
-          "markdown_inline",
-        },
-        -- Install parsers synchronously (only for ensure_installed)
-        sync_install = true,
-        -- Enable highlighting
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        -- Optional: Enable other Treesitter modules
-        indent = {
-          enable = true,
-        },
-      })
+
+    opts = {
+      ensure_installed = {
+        "bash","c","cmake","cpp","cuda","lua","python","rust",
+        "vim","vimdoc","query","markdown","markdown_inline",
+      },
+      sync_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+    },
+
+    config = function(_, opts)
+      local ok, configs = pcall(require, "nvim-treesitter.configs")
+      if not ok then
+        return
+      end
+      configs.setup(opts)
     end,
   },
 }
